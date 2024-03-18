a24e3da813ad17e1eaaaa6331b4ce1e49066f1b5
GustavoLVJardim <157443428+GustavoLVJardim@users.noreply.github.com>
2024-03-17 21:32:46 -0300

    Add files via upload

---
+++ 9_cumprimentos_3.2._.py
@@ -0,0 +1,16 @@
+"""Cumprimentos: Comece com a lista usada no exercício anterior,
+mas em vez de apenas exibir o nome de cada pessoa, exiba também
+uma mensagem para elas. O texto de cada mensagem deve ser o mesmo,
+porém, cada mensagem deve ser personalizada com o nome da pessoa"""
+
+# criando a lista names e inserindo os nomes de alguns familiares:
+names = ['Joyce', 'Lis', 'Kênia', 'Agnaldo', 'Luana']
+print(names)
+print('')
+
+# retornando com a mensagem e o nome de cada uma das pessoas da lista:
+print('Olá, como é bom ter você em minha família', names[0])
+print('Olá, como é bom ter você em minha família', names[1])
+print('Olá, como é bom ter você em minha família', names[2])
+print('Olá, como é bom ter você em minha família', names[3])
+print('Olá, como é bom ter você em minha família', names[4])
