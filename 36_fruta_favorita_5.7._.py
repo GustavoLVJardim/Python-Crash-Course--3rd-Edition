518a2f386d9421e4ad063e1a6c1add98c71ba150
Gustavo L V Jardim <157443428+GustavoLVJardim@users.noreply.github.com>
2024-05-27 22:50:39 -0300

    Add files via upload

---
+++ 36_fruta_favorita_5.7._.py
@@ -0,0 +1,20 @@
+"""Fruta favorita: Crie uma lista de suas frutas favoritas e, em seguida, escreva uma série de declarações
+if independentes qe verificam determindas frutas em sua lista:
+- crie uma lista com sua três frutas favoritas e a nomeie como favorite_fruits;
+- escreva cinco instruções if. Cada uma deve verificar se um determinado tipo de fruta
+consta em sua lista. Se sim, o bloco if deve exibir uma afirmação do tipo:
+exemplo: você realmente gosta de bananas!
+"""
+
+favorite_fruits = ['abacate', 'limao', 'morango',]
+
+if 'banana' in favorite_fruits:
+    print('Você realmente gosta de bananas')
+if 'melancia' in favorite_fruits:
+    print('Você realmente gosta de melancia')
+if 'abacate' in favorite_fruits:
+    print('Você realmente gosta de abacate')
+if 'limao' in favorite_fruits:
+    print('Você realmente gosta de limao')
+if 'morango' in favorite_fruits:
+    print('Você realmente gosta de morango')
