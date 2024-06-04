0d51b885598e157ffd25dffb8988c8dea2abd476
Gustavo L V Jardim <157443428+GustavoLVJardim@users.noreply.github.com>
2024-06-03 21:37:04 -0300

    Add files via upload

---
+++ 48_animais_de_estimacao_6.8._.py
@@ -0,0 +1,6 @@
+"""Animais de estimação: Crie vários dicionários, em que cada dicionário representa
+um animal de estimação diferente. Em cada dicionário inclua o tipo de animal  e o
+nome do dono. Armazene esses dicionários em uma lista chamada pets. Depois, percorra
+a sua lista com um loop e, enquanto faz isso, exiba tudo o que sabe sobre cada animal
+de estimação.
+"""
