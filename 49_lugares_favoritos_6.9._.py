091ed0b91fd60a62908d42af38f11a4f0b7bf361
Gustavo L V Jardim <157443428+GustavoLVJardim@users.noreply.github.com>
2024-06-03 21:38:22 -0300

    Add files via upload

---
+++ 49_lugares_favoritos_6.9._.py
@@ -0,0 +1,6 @@
+"""Lugares favoritos: Crie um dicionário favorites_places. Pense em três nomes
+para usar como chave no dicionário e armazene de um a três lugares favoritos
+para cada pessoa. Agora, para que este exercício fique ainda mais interessante,
+peça a alguns amigos que lhe digam alguns de seus lugares favortos. Percorra o
+dicionário com um loop e exiba o nome de cada pessoa e seus lugares favoritos.
+"""
