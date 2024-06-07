712c6a8731a99cb5d0678803155a4391b4938892
Gustavo L V Jardim <157443428+GustavoLVJardim@users.noreply.github.com>
2024-06-06 23:18:40 -0300

    Add files via upload

---
+++ 52_extensoes_6.12._.py
@@ -0,0 +1,5 @@
+"""Extensões: Agora que já estamos trabalhando com exemplos complexos o suficiente
+para que sejam mais desenvolvidos de diferentes maneiras, use um dos programas de exemplo
+do capítulo 6 e o amplie, adicionando chaves e valores novos, alterando o contexto ou
+melhorando a formatação da saída.
+"""
