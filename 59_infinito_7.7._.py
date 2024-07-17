e747104296f3941a6b0c97926876a82a67262a7d
Gustavo L V Jardim <157443428+GustavoLVJardim@users.noreply.github.com>
2024-07-16 23:14:20 -0300

    Add files via upload

---
+++ 59_infinito_7.7._.py
@@ -0,0 +1,7 @@
+"""Infinito: Escreva e execute um loop infinito (para encerrar o loop
+pressione CTRL + C ou feche a janela que exibe a saída).
+"""
+
+while True:
+    name = input('Seu nome: ')
+    print(name)
