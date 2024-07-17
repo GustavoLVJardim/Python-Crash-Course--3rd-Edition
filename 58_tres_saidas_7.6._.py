334feb1f8d07d5f034a34157b910d04c685642be
Gustavo L V Jardim <157443428+GustavoLVJardim@users.noreply.github.com>
2024-07-16 22:06:33 -0300

    Add files via upload

---
+++ 58_tres_saidas_7.6._.py
@@ -0,0 +1,6 @@
+"""Três Saídas: Crie diferentes versões do exercício 7.4. ou 7.5.
+que executem cada uma das seguintes tarefas, pelo menos, uma vez:
+- use um teste condicional na instrução while para interromper o loop;
+- use uma variável 'active' para controlar o tempo que o loop é executado;
+- use uma instrução 'break' para sair do loop quando o usuário inserir o valor 'quit';
+"""
