278fdac976ea1b38b10c4a6122d255d8488ea9e9
Gustavo L V Jardim <157443428+GustavoLVJardim@users.noreply.github.com>
2024-07-18 20:25:36 -0300

    Add files via upload

---
+++ 66_camisetas_grandes_8.4._.py
@@ -0,0 +1,9 @@
+# Camisetas grandes: Modifique a função make_shirt() para que as camisetas sejam grandes por padrão, #com a seguinte frase estampada: Eu amo Python. Escreva uma camiseta grande e uma média com a mensagem 
+#padrão e uma camiseta de qualquer tamnhanho com uma frase diferente:
+
+def make_shirt(size, text):
+    print(f'Size: {size}\nText: "{text}"')
+
+
+make_shirt('X', 'I Love Python')
+    
