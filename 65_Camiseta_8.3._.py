494cf1d8b0c6a58c1dedb9f2710ba4c4032bcf12
Gustavo L V Jardim <157443428+GustavoLVJardim@users.noreply.github.com>
2024-07-18 20:19:53 -0300

    Add files via upload

---
+++ 65_Camiseta_8.3._.py
@@ -0,0 +1,9 @@
+# Camiseta: Crie uma funbção chamada make_shirt() que aceita um tamanho e o texto que deve ser #estampado na camiseta. A função deve exibir uma frase resumindo o tamanho da camiseta e a mensagem
+# estampada nela.
+
+def make_shirt(size, text):
+    print(f'Size: {size}\nText: "{text}"')
+
+
+make_shirt('XL', 'insider')
+
