c8d8c0c6b67d228ec27ebed9eb518e01ac8309fe
Gustavo L V Jardim <157443428+GustavoLVJardim@users.noreply.github.com>
2024-07-18 20:18:04 -0300

    Add files via upload

---
+++ 63_mensagem_8.1._.py
@@ -0,0 +1,7 @@
+# """Mensagem: escreva uma função chamada display_message() que exiba uma frase contanto a todo mundo #que você está aprendendo neste capítulo. Chame a função e veirifque se a mensagem é adequadamente #exibida.
+# """
+
+def display_message():
+    print('Estou aprendendo a programar em python.')
+
+display_message()
