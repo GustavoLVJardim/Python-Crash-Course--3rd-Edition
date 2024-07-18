17a454c71a77d62495eaafb7e7fa64461f06453e
Gustavo L V Jardim <157443428+GustavoLVJardim@users.noreply.github.com>
2024-07-18 20:18:51 -0300

    Add files via upload

---
+++ 64_livro_favorito_8.2._.py
@@ -0,0 +1,9 @@
+# Livro Favorito: Escreva uma função chamada favorite_book() que aceite um parâmetro, title.
+# A função deve exibir uma mensagem como: um dos meus livros favoritos é Alice no País das Maravilhas.
+# Chame a função e lembre-se de incluir o título como argumento na chamada da função.
+
+def favorite_book(title):
+    print(f'Um dos meus livros favoritos é {title}')
+
+
+favorite_book('Crime e Castigo')
